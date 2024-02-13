eec009870a98286bf6a558389c436eba966c6130
Francisco Rozas <francisco.rozas@outlook.com>
2024-02-13 15:16:18 -0300

    first commit

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -15,13 +15,13 @@ export const AppRoutes = () => {
   return (
     <Routes>
         <Route path='/*' element={<App/>}/>
-        <Route path='Medicpro_Fron/Generar-adquisicion-asia' element={<Adquisicion/>}/>
-        <Route path="Medicpro_Fron/Generar-adquisicion-America" element={<AdquisicionAmerica/>}/>
-        <Route path="Medicpro_Fron/Lista-Productos" element={<ListaProductos/>}/>
-        <Route path="Medicpro_Fron/Manejo-de-Stock" element={<ListaProductosEdit/>}/>
-        <Route path='Medicpro_Fron/Seguimiento-Pedidos' element={<SeguimientoAdquisicion/>}/>
-        <Route path='Medicpro_Fron/Ingreso-Ventas' element={<IngresarVenta/>}/>
-        <Route path='Medicpro_Fron/Adm.-de-Facturas' element={<ListadeOrdenCompra/>}/>
+        <Route path='/Generar-adquisicion-asia' element={<Adquisicion/>}/>
+        <Route path="/Generar-adquisicion-America" element={<AdquisicionAmerica/>}/>
+        <Route path="/Lista-Productos" element={<ListaProductos/>}/>
+        <Route path="/Manejo-de-Stock" element={<ListaProductosEdit/>}/>
+        <Route path='/Seguimiento-Pedidos' element={<SeguimientoAdquisicion/>}/>
+        <Route path='/Ingreso-Ventas' element={<IngresarVenta/>}/>
+        <Route path='/Adm.-de-Facturas' element={<ListadeOrdenCompra/>}/>
         
         <Route path='/paginaError' element={<App/>}/>
         
--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {AppRoutes} from './Routes/AppRoutes.jsx'
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 import {Sidebar} from './Components/SideBar.jsx'
-
+import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
    
    
-    <BrowserRouter>
+    <HashRouter>
     <Sidebar/>
     <div className='bloque-layout'><AppRoutes /></div>
-    </BrowserRouter>
+    </HashRouter>
   
   //h</React.StrictMode>
 );
